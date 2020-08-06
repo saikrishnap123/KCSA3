7f8a32a277f5c107cc632c54647c456bcd6e1184
sai krishna <sai krishna@LAPTOP-KV1JTPOJ>
2020-08-06 17:22:42 +0530

    commit

---
+++ src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,16 @@
+package com.actitime.timetrack;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+	
+	@Test
+	public void demo()
+	{
+		Reporter.log("succcessfully done", true);
+		Reporter.log("succcessfully done", true);
+		Reporter.log("succcessfully done", true);
+	}
+
+}
